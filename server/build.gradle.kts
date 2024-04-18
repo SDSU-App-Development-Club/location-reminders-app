45916698040f0955af57b843a367bbaee945ea69
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-04-17 18:09:19 -0700

    Add SprintBoot and H2 for database

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,4 +18,11 @@ dependencies {
     implementation(libs.ktor.server.netty)
     testImplementation(libs.ktor.server.tests)
     testImplementation(libs.kotlin.test.junit)
+
+    implementation(libs.spring.boot.starter)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.kotlin.reflect)
+
+    implementation(libs.h2)
+    implementation(libs.exposed.spring.boot.starter)
 }
