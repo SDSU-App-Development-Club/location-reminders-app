4287c669f54230c31bfef8a3936a5a27d03f8100
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-05-14 10:12:58 -0700

    Force LATEST map renderer

--- composeApp/src/androidMain/kotlin/swifties/testapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/swifties/testapp/MainActivity.kt
@@ -7,11 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.MapsInitializer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        MapsInitializer.initialize(applicationContext)
         setContent {
             MaterialTheme {
                 App()
