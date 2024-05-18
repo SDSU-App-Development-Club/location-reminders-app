9cf612ea71abcf6688b701ad214c8aebe15cc0dc
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-05-17 23:28:30 -0700

    testing

--- server/src/main/kotlin/swifties/testapp/Tables.kt
+++ server/src/main/kotlin/swifties/testapp/Tables.kt
@@ -1,15 +1,15 @@
 package swifties.testapp
 
-import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.kotlin.datetime.CurrentDateTime
+import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
-object LocationAlerts : IntIdTable() {
-    object LocationAlerts : Table() {
-        val alertId = integer("alert_id").autoIncrement().primaryKey()
-        val locationName = varchar("location_name", 255)
-        val latitude = double("latitude")
-        val longitude = double("longitude")
-        val radius = integer("radius")
-        val message = text("message")
-        val active = bool("active").default(true)
-        val createdAt = datetime("created_at").defaultExpression(CurrentDateTime())
+object LocationAlerts : IntIdTable(columnName = "alert_id") {
+    val locationName = varchar("location_name", 255)
+    val latitude = double("latitude")
+    val longitude = double("longitude")
+    val radius = integer("radius")
+    val message = text("message")
+    val active = bool("active").default(true)
+    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
 }
