bf2932307a7b1f7f44bbd61787a37b914d91ba84
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-05-21 22:41:29 -0700

    Code cleanup

--- server/src/main/kotlin/swifties/testapp/Application.kt
+++ server/src/main/kotlin/swifties/testapp/Application.kt
@@ -1,39 +1,38 @@
 package swifties.testapp
 
-import Greeting
 import io.github.cdimascio.dotenv.Dotenv
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.from
 import io.ktor.serialization.jackson.jackson
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.request.receive
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.deleteWhere
-import org.jetbrains.exposed.sql.transactions.transaction
+import io.ktor.server.response.respond
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.put
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import io.ktor.client.*
-import io.ktor.client.engine.java.*
-import javax.management.Query.eq
-import javax.xml.stream.Location
 
+val logger: Logger = LoggerFactory.getLogger("intro-app")
+val env: Dotenv = Dotenv.load()
 
-val logger = LoggerFactory.getLogger("pingas")
-val env = Dotenv.load()
 fun main() {
     embeddedServer(Netty, port = 8080) {
         install(ContentNegotiation) {
             jackson()
         }
-        val client = HttpClient(Java)
+
         val supabase: SupabaseClient
-        // just for testing, points to a local docker instance
+
         try {
             supabase = createSupabaseClient(
                 supabaseUrl = env.get("DB_URL"),
@@ -46,12 +45,6 @@ fun main() {
             logger.info("Cannot connect to Supabase", e)
             throw e
         }
-
-
-        // make the table in Postgres
-//        transaction {
-//            SchemaUtils.create(AlertsTable)
-//        }
 
         routing {
             route("/alerts") {
@@ -72,7 +65,7 @@ fun main() {
                         return@get
                     }
                     try {
-                        val alert = supabase.from("location_alerts").select() {
+                        val alert = supabase.from("location_alerts").select {
                             filter {
                                 LocationAlert::alert_id eq id
                             }
@@ -123,12 +116,4 @@ fun main() {
             }
         }
     }.start(wait = true)
-}
-
-fun Application.module() {
-    routing {
-        get("/") {
-            call.respondText("Ktor: ${Greeting().greet()}")
-        }
-    }
 }
--- server/src/main/kotlin/swifties/testapp/Models.kt
+++ server/src/main/kotlin/swifties/testapp/Models.kt
@@ -1,15 +1,19 @@
 package swifties.testapp
 
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LocationAlert(
-        val alert_id: Int,
-        val location_name: String,
-        val latitude: Double,
-        val longitude: Double,
-        val radius: Int,
-        val message: String,
-        val active: Boolean = true,
-        val created_at: String? = null
+@OptIn(ExperimentalSerializationApi::class)
+data class LocationAlert constructor(
+    @EncodeDefault(EncodeDefault.Mode.NEVER)
+    val alert_id: Int = 0,
+    val location_name: String,
+    val latitude: Double,
+    val longitude: Double,
+    val radius: Int,
+    val message: String,
+    val active: Boolean = true,
+    val created_at: String? = null
 )
