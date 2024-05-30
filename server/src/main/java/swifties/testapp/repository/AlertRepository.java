f5515d03b59b1c32540f00c2dd645424f0ebd0be
Cameron Cobb <cameroncobb411@outlook.com>
2024-05-29 18:26:52 -0700

    authentication setup

--- server/src/main/java/swifties/testapp/repository/AlertRepository.java
+++ server/src/main/java/swifties/testapp/repository/AlertRepository.java
@@ -1,8 +1,15 @@
 package swifties.testapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import swifties.testapp.dtos.LoginUserDto;
+import swifties.testapp.dtos.RegisterUserDto;
 import swifties.testapp.entity.Alert;
+import swifties.testapp.entity.User;
 
 @Repository
 public interface AlertRepository extends JpaRepository<Alert, Integer> {
