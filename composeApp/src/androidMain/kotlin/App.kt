9e0dc568dff7d8a300b5b5b46225a8cd35ba3029
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-06-05 23:02:20 -0700

    Fix Log in screen

--- composeApp/src/androidMain/kotlin/App.kt
+++ composeApp/src/androidMain/kotlin/App.kt
@@ -1,4 +1,3 @@
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,6 +10,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 const val SIGNUP_SCREEN_ROUTE = "signup_screen"
 const val LOGIN_SCREEN_ROUTE = "login_screen"
 const val DASHBOARD_SCREEN_ROUTE = "dashboard_screen"
+
 @Composable
 @Preview
 fun App(
@@ -24,18 +24,22 @@ fun App(
             .fillMaxSize()
     ) {
         composable(SIGNUP_SCREEN_ROUTE) {
-            SignUpScreen { token ->
-                navController.navigate("$DASHBOARD_SCREEN_ROUTE/$token")
-            }
+            SignUpScreen(
+                { token -> navController.navigate("$DASHBOARD_SCREEN_ROUTE/$token") },
+                { navController.navigate(LOGIN_SCREEN_ROUTE) },
+            )
         }
-        /*composable(LOGIN_SCREEN_ROUTE) {
-            LogInScreen()
-        }*/
+        composable(LOGIN_SCREEN_ROUTE) {
+            LogInScreen(
+                { token -> navController.navigate("$DASHBOARD_SCREEN_ROUTE/$token") },
+                { navController.navigate(SIGNUP_SCREEN_ROUTE) },
+            )
+
+        }
         composable("$DASHBOARD_SCREEN_ROUTE/{token}") { backStackEntry ->
             val userId: String = backStackEntry.arguments?.getString("token")!!
             DashboardScreen(userId)
         }
-
     }
 }
 
