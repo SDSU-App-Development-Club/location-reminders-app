16fba370e7c151c22d643814d4391ef45584f4c5
deanna <domron4245@sdsu.edu>
2024-06-09 22:39:47 -0700

    added the login and sign up page

---
+++ iosApp/iosApp/LoginInScreen.swift
@@ -0,0 +1,19 @@
+//
+//  LoginInScreen.swift
+//  iosApp
+//
+//  Created by Deanna O on 6/9/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginInScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginInScreen()
+}
---
+++ iosApp/iosApp/SignUpScreen.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpScreen.swift
+//  iosApp
+//
+//  Created by Deanna O on 6/9/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignUpScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpScreen()
+}
