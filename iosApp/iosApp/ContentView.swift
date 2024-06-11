862851a86a94f93cf51c6b2810e2d559af991730
deanna <domron4245@sdsu.edu>
2024-06-10 23:23:05 -0700

    got navigation between welcome page and login screen

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,75 +4,53 @@ import Shared
 struct ContentView: View {
     @State private var showContent = false
     var body: some View {
+        NavigationView {
+            NavigationLink {
+                // destination view to navigate to
+                LogInScreen()
+            } label: {
+                ZStack {
+                    Image("Welcome Page")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    // holds decorative squiggly lines
+                    VStack {
+                        Image("Welcome Page Squiggly 1")
+                            .offset(y: -110)
+                        
+                        Image("Welcome Page Squiggly 2")
+                            .offset(x: -170, y: 30)
+                        
+                        Image("Welcome Page Squiggly 3")
+                            .offset(y: 50)
+                    }
+                    
+                    VStack {
+                        
+                        Spacer()
+                        
+                        Text("Welcome!")
+                            .fontWeight(.black)
+                            .font(.system(size: 48))
+                            .frame(maxWidth: 300, maxHeight: 75, alignment: .center)
+                            .foregroundColor(Color(hex: "#FFFFFF"))
+                        
+                        Image("PeaceSign")
+                            .padding()
 
-        // background of the welcome page
-       ZStack  {
-            Image ("Welcome Page")
-
-
-            // holds decorative squiggly lines
-             VStack {
-                Image ("Welcome Page Squiggly 1.png")
-                    .offset(y: -110)
-
-                Image ("Welcome Page Squiggly 2.png")
-                    .offset(x: -170, y: 30)
-
-                Image ("Welcome Page Squiggly 3.png")
-                    .offset(y: 50)
-
-            }
-
-
-            // holds text and icon welcoming people to the app
-            VStack {
-                //text needs to be arial
-                //fix spacing
-                //fix font weight
-                Text("Welcome!")
-                    .fontWeight(.black)
-                    .font(.system(size: 48))
-                    .padding()
-                    .frame(maxWidth: 300, maxHeight: 75, alignment: .center)
-                    .foregroundColor(Color(hex: "#FFFFFF"))
-
-                Image ("PeaceSign")
-
-                Button(action: {}) {
-                    Text("Tap anywhere to begin")
-                        .frame(maxWidth: 270, alignment: .center)
-                        //.fontWeight(.black)
-                        .font(.title2)
-                        .padding()
-                        .foregroundColor(Color(hex: "#FFFFFF"))
+                        
+                        Text("Tap anywhere to begin")
+                            .frame(maxWidth: 300, alignment: .center)
+                            .foregroundColor(Color(hex: "#FFFFFF"))
+                            .font(.title2)
+                        
+                        Spacer()
+                    }
                 }
-
             }
         }
-
     }
-
-//    var body: some View {
-//        VStack {
-//            Button("Click me!") {
-//                withAnimation {
-//                    showContent = !showContent
-//                }
-//            }
-//
-//            if showContent {
-//                VStack(spacing: 16) {
-//                    Image(systemName: "swift")
-//                        .font(.system(size: 200))
-//                        .foregroundColor(.accentColor)
-//                    Text("SwiftUI:")
-//                }
-//                .transition(.move(edge: .top).combined(with: .opacity))
-//            }
-//        }
-//        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-//        .padding()
-//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -80,3 +58,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- iosApp/iosApp/LoginInScreen.swift
+++ iosApp/iosApp/LoginInScreen.swift
@@ -8,12 +8,103 @@
 
 import SwiftUI
 
-struct LoginInScreen: View {
+struct LogInScreen: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack { //background
+            
+            Image("Sign Up Background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("Login Squiggly2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .offset(y: -0)
+                
+                Spacer()
+                
+                Image("Login Squiggly1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                    .offset(x: 0, y: -20)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                VStack {
+                    Text("Log In")
+                        .fontWeight(.bold)
+                        .font(.system(size: 36))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: 300, alignment: .leading)
+
+                }
+                    .offset(y: -20)
+                
+                
+                VStack { //input email and password to sign up
+                    TextField("Email Address", text: $email)
+                        .offset(x: 15)
+                        .frame(maxWidth: 300, maxHeight: 40, alignment: .trailing)
+                        .background(Color.white)
+                        .cornerRadius(15.0)
+                        .offset(y: -30)
+                        .font(.system(size: 16))
+                        
+                    
+                    
+                    TextField("Password", text: $password)
+                        .offset(x: 15)
+                        .frame(maxWidth: 300, maxHeight: 40, alignment: .trailing)
+                        .background(Color.white)
+                        .cornerRadius(15.0)
+                        .offset(y: -20)
+                        .font(.system(size: 16))
+                    
+                    Button (action: {}) { //create account button
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(width: 300, height: 40) // Adjust the frame size as needed
+                                .foregroundColor(.white)
+                            
+                            Text("Log In")
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                            //fix coloring and add a drop shadow
+                            
+                        }
+                        .offset(y: -10)
+                    }
+                    
+                    Button (action: {}) {
+                        Text ("Already have an account?")
+                            .foregroundColor(.white)
+                        + Text(" Log in.")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                    .frame(width: 290, height: 20, alignment: .leading)
+                    .font(.system(size: 16))
+                    
+                }
+                
+            }
+            .offset(y: -40)
+            
+        }
     }
+    
 }
 
-#Preview {
-    LoginInScreen()
+
+struct LogInScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInScreen()
+    }
 }
