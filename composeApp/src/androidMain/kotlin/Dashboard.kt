f7924ad8b5b7bc81657d048bc063306d1c8ec01e
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-06-13 23:55:56 -0700

    Full screen popup menu inside Dashboard

--- composeApp/src/androidMain/kotlin/Dashboard.kt
+++ composeApp/src/androidMain/kotlin/Dashboard.kt
@@ -1,12 +1,13 @@
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
-import androidx.compose.animation.with
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,13 +17,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FloatingActionButtonDefaults.elevation
 import androidx.compose.material.Icon
 import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
@@ -35,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -43,69 +46,68 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun DashboardScreen(userId: String) {
     Box(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
         GradientImageView()
         ScheduleScreen()
     }
 }
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun ScheduleScreen() {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp),
-
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
     ) {
-        Spacer(modifier = Modifier.height(20.dp))
-        DaysOfWeekRow()
-        Spacer(modifier = Modifier.height(32.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Today",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                modifier = Modifier.alignByBaseline()
-            )
+        var showPopup by remember { mutableStateOf(false) }
+        val showList = remember {
+            MutableTransitionState(true)
         }
-        Divider(
-            color = Color.White,
-            thickness = 1.dp,
-            modifier = Modifier
-                .padding(vertical = 8.dp)
-                .fillMaxWidth(0.9f)
-        )
-        Spacer(modifier = Modifier.height(60.dp))
-        Text(
-            text = "No new reminders",
-            fontSize = 18.sp,
-            color = Color.White
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(
-            shape = RoundedCornerShape(20.dp),
-            onClick = { /* Handle new task creation */ },
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
-        ) {
-            Text(text = "+ New Task", color = Color.Black)
-        }
-        Spacer(modifier = Modifier.weight(1f))
 
-        var isClicked by remember { mutableStateOf(false) }
+        Spacer(modifier = Modifier.height(50.dp))
+        AnimatedVisibility(visibleState = showList) {
+            Column(modifier = Modifier.padding(15.dp)) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Today",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier.alignByBaseline()
+                    )
+                }
+                Divider(
+                    color = Color.White,
+                    thickness = 1.dp,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .fillMaxWidth(0.9f)
+                )
+                Spacer(modifier = Modifier.height(60.dp))
+                Text(
+                    text = "No new reminders",
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    shape = RoundedCornerShape(20.dp),
+                    onClick = { /* Handle new task creation */ },
+                    colors = buttonColors(backgroundColor = Color.White),
+                ) {
+                    Text(text = "+ New Task", color = Color.Black)
+                }
+            }
+        }
 
         AnimatedContent(
-            targetState = isClicked,
+            targetState = showPopup,
             transitionSpec = {
-                val durationMillis = 1000
+                val durationMillis = 500
 
                 if (targetState != initialState && targetState) {
                     slideInVertically(animationSpec = tween(durationMillis)) { height -> height } togetherWith
@@ -114,28 +116,55 @@ fun ScheduleScreen() {
                     slideInVertically(animationSpec = tween(durationMillis)) { height -> -height } togetherWith
                             slideOutVertically(animationSpec = tween(durationMillis)) { height -> height }
                 }.using(SizeTransform(clip = false))
-            }, label = "Add Alert Popup Menu"
+            },
+            label = "Add Alert Popup Menu",
+            modifier = Modifier.fillMaxSize()
         ) { buttonClicked ->
             if (buttonClicked) {
-                Column(Modifier.fillMaxSize()) {
-                    Text(
-                        "Some content",
+                // Show entire menu
+                Box {
+                    Column(
                         modifier = Modifier
                             .fillMaxSize()
-                            .background(Color.White)
-                            .padding(top = 100.dp),
-                        fontSize = 26.sp
-                    )
+                            .padding(top = 32.dp)
+                            .clip(RoundedCornerShape(topEnd = 20.dp))
+                    ) {
+                        Text(
+                            "Some content",
+                            modifier = Modifier
+                                .background(Color.White)
+                                .padding(top = 100.dp)
+                                .fillMaxSize(),
+                            fontSize = 26.sp
+                        )
+                    }
+                    FloatingActionButton(
+                        onClick = {
+                            showPopup = false
+                            showList.targetState = true
+                        },
+                        contentColor = Color.White,
+                        backgroundColor = Color(0xFF009a88),
+                        elevation = elevation(0.dp, 0.dp),
+                        shape = CircleShape,
+                        modifier = Modifier
+                            .size(64.dp)
+                            .border(6.dp, Color.White, CircleShape),
+                    ) {
+                        Icon(Icons.Default.Add, contentDescription = "Add Task")
+                    }
                 }
             } else {
-                // Add Button at bottom of screen
                 FloatingActionButton(
-                    onClick = { isClicked = true },
+                    onClick = {
+                        showPopup = true
+                        showList.targetState = false
+                    },
                     backgroundColor = Color.White,
                     contentColor = Color(0xFF4CAF50),
                     modifier = Modifier
-                        .padding(16.dp)
-                        .background(Color.White)
+                        .requiredSize(50.dp)
+                        .align(Alignment.CenterHorizontally),
                 ) {
                     Icon(Icons.Default.Add, contentDescription = "Add Task")
                 }
@@ -143,49 +172,3 @@ fun ScheduleScreen() {
         }
     }
 }
-
-@Composable
-fun DaysOfWeekRow() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-            .background(color = Color.DarkGray),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        val daysOfWeek = listOf("S", "M", "T", "W", "T", "F", "S")
-        daysOfWeek.forEachIndexed { index, day ->
-            DayItem(day, index + 1)
-        }
-    }
-}
-
-@Composable
-fun DayItem(day: String, date: Int) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = day,
-            color = Color.White,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Box(
-            modifier = Modifier
-                .size(36.dp)
-                .background(Color.White, shape = CircleShape),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = date.toString(),
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-    }
-}
-
-
