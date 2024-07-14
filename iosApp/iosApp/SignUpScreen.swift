ff209f324a0658d1ec35125f701309711a1d337c
deanna <deanna@cs470.local>
2024-07-13 23:07:32 -0700

    fixed navigation on SignUpScreen and got auth to work

--- iosApp/iosApp/SignUpScreen.swift
+++ iosApp/iosApp/SignUpScreen.swift
@@ -7,98 +7,67 @@
 //
 
 
+
+
 import SwiftUI
 import Shared
+
 
 struct SignUpScreen: View {
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var signInSuccess = false
+    @State private var needLogIn = false
+    
+    
     var body: some View {
+        Group {
+            
             ZStack { //background
             
-                Image("Sign Up Background")
-                    .resizable()
-                    .edgesIgnoringSafeArea(.all)
+                BackgroundWallpaper()
                 
-                VStack {
-                    Image("Sign Up Page Squiggly 1")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxHeight: .infinity, alignment: .top)
-                        .offset(y: -90)
-
-                    Spacer()
-
-                    Image("Sign Up Page Squiggly 2")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxHeight: .infinity, alignment: .bottom)
-                        .offset(x: 20, y: 60)
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .edgesIgnoringSafeArea(.all)
+                BackgroundDetails()
                 
-                VStack {
-                    SignUpText()
-                        .offset(y: -20)
-                       
-                    
-                    VStack { //input email and password to sign up
-                        TextField("Email Address", text: $email)
-                            .offset(x: 15)
-                            .frame(maxWidth: 300, maxHeight: 50, alignment: .trailing)
-                            .background(Color.white)
-                            .cornerRadius(18.0)
-                            .offset(y: -30)
-
-                        
-                        SecureField("Password", text: $password)
-                            .offset(x: 15)
-                            .frame(maxWidth: 300, maxHeight: 50, alignment: .trailing)
-                            .background(Color.white)
-                            .cornerRadius(18.0)
-                            .offset(y: -20)
-                        
-                        Button (action: {
-                            RestAPIAccess().attemptSignup(username: email, password: password) { response, error in
-                                if let response = response {
-                                    print(response)
-                                }
-                            }
-                        }) { //create account button
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 18)
-                                    .frame(width: 300, height: 50) // Adjust the frame size as needed
-                                    .foregroundColor(.white)
+                if signInSuccess {
+                    LogInScreen()
+                } else {
+                    //LoginView()
+                    if needLogIn {
+                        LogInScreen()
+                    } else {
+                        VStack {
+                            SignUpText()
+                                .offset(y: -20)
+                               
+                            
+                            VStack { //input email and password to sign up
                                 
-                                Text("Create Account")
-                                    //fix coloring and add a drop shadow
+                                Email(email: $email)
+                                
+                                Password(password: $password)
+                               
+                                CreateAccountButton(email: $email, password: $password, signInSuccess: $signInSuccess)
+                                
+                                //turn into a button
+                                AlreadyHaveAnAccount(needLogIn: $needLogIn)
+
                             }
-                            .offset(y: -10)
+                            
                         }
-                        
-                        NavigationLink {
-                            // destination view to navigate to
-                            LogInScreen()
-                        } label: {
-                            Text ("Already have an account?")
-                                .foregroundColor(.white)
-                            + Text(" Log in.")
-                                .foregroundColor(.white)
-                                .bold()
-                        }
-                        .frame(width: 290, height: 20, alignment: .leading)
-                        .font(.system(size: 16))
-                    
-                       
+                        .offset(y: -40)
                     }
-                   
-                }
-                .offset(y: -40)
                 
+                }
+
             }
+        }
+           
     }
+    
+    
 }
+
 
 struct SignUpText: View {
     var body: some View {
@@ -116,5 +85,103 @@ struct SignUpText: View {
 struct SignUpScreen_Previews: PreviewProvider {
     static var previews: some View {
         SignUpScreen()
+    }
+}
+
+struct  BackgroundDetails: View {
+    var body: some View {
+        VStack {
+            Image("Sign Up Page Squiggly 1")
+                .resizable()
+                .scaledToFit()
+                .frame(maxHeight: .infinity, alignment: .top)
+                .offset(y: -90)
+            
+            Spacer()
+            
+            Image("Sign Up Page Squiggly 2")
+                .resizable()
+                .scaledToFit()
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                .offset(x: 20, y: 60)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct BackgroundWallpaper: View {
+    var body: some View {
+        Image("Sign Up Background")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct Email: View {
+    @Binding var email: String
+    var body: some View {
+        TextField("Email Address", text: $email)
+            .offset(x: 15)
+            .frame(maxWidth: 300, maxHeight: 50, alignment: .trailing)
+            .background(Color.white)
+            .cornerRadius(18.0)
+            .offset(y: -30)
+            .autocapitalization(.none)
+    }
+}
+
+struct Password: View {
+    @Binding var password: String
+    var body: some View {
+        SecureField("Password", text: $password)
+            .offset(x: 15)
+            .frame(maxWidth: 300, maxHeight: 50, alignment: .trailing)
+            .background(Color.white)
+            .cornerRadius(18.0)
+            .offset(y: -20)
+    }
+}
+
+struct CreateAccountButton: View {
+    @Binding var email: String
+    @Binding var password: String
+    @Binding var signInSuccess: Bool
+    var body: some View {
+        Button(action: {
+            RestAPIAccess().attemptSignup(username: email, password: password) {
+                response, error in
+                if let response = response {
+                    signInSuccess = true
+                }
+                
+            }
+        }){
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 18)
+                    .frame(width: 300, height: 50) // Adjust the frame size as needed
+                    .foregroundColor(.white)
+                
+                Text("Create Account")
+                //fix coloring and add a drop shadow
+            }
+            .offset(y: -10)
+        }
+    }
+}
+
+struct AlreadyHaveAnAccount: View {
+    @Binding var needLogIn: Bool
+    var body: some View {
+        Button(action: {
+            needLogIn = true
+        }){
+            Text ("Already have an account?")
+                .foregroundColor(.white)
+            + Text(" Log in.")
+                .foregroundColor(.white)
+                .bold()
+        }
     }
 }
